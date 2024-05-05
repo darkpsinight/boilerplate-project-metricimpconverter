832804e638e238c28515eb0e0cdf061c7ebaca49
darkpsinight <darkpsinight@hotmail.com>
2024-05-05 20:31:00 +0100

    api route, convert handlers and helpers functions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,8 +1,39 @@
+// Two helpers functions:
+function regexSplit(input) {
+  const matches = input.match(/([.\d\/]+) | ([a-zA-Z]+)/g);
+
+  const number = matches
+    ? matches.find((match) => /[.\d\/]+/.test(match))
+    : "1";
+  const string = matches
+    ? matches.find((match) => /[a-zA-Z]+/.test(match))
+    : "";
+
+  return [number, string];
+}
+
+function onlyOneDiv(input) {
+  let fractionParts = input.split("/");
+  if (fractionParts.length > 2) {
+    return false;
+  }
+  return fractionParts;
+}
+
 function ConvertHandler() {
   this.getNum = function (input) {
-    let result;
+    let result = regexSplit(input)[0];
+    let fractionArray = onlyOneDiv(result);
 
-    return result;
+    if (
+      !fractionArray ||
+      isNaN(parseFloat(fractionArray[0])) ||
+      isNaN(parseFloat(fractionArray[1] || "1"))
+    ) {
+      return undefined;
+    }
+
+    return parseFloat(fractionArray[0]) / parseFloat(fractionArray[1] || "1");
   };
 
   this.getUnit = function (input) {
